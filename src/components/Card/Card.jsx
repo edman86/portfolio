ac22c665684ae6cc876463bbfff1cd37a32bc897
edman <compFoxLord007@gmail.com>
2021-11-10 23:31:03 +0400

    fixed design

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,4 @@
 import { AiFillGithub } from 'react-icons/ai';
-import { BiRightArrow } from 'react-icons/bi';
 import './card.scss';
 
 const Card = ({ name, image, description, appLink, repoLink }) => {
@@ -7,7 +6,6 @@ const Card = ({ name, image, description, appLink, repoLink }) => {
         <article className="card">
             <header className="card__image-container">
                 <img className="card__image" src={image} alt={name} />
-                <div className="card__visual-filter"></div>
             </header>
             <main className="card__content">
                 <h2 className="card__title">{name}</h2>
@@ -20,18 +18,14 @@ const Card = ({ name, image, description, appLink, repoLink }) => {
                 </ul>
             </main>
             <footer className="card__footer">
+                <a className="link" href={repoLink}>
+                    <AiFillGithub size="2em" className="github-icon" />
+                </a>    
                 <a className="link" href={appLink}>
-                    <button type="button" className="card__button border-animation">
-                        <BiRightArrow size="1.5em" />
-                        <span className="label">Open Project</span>
+                    <button type="button" className="card__button">
+                        View project
                     </button>
                 </a>
-                <a className="link" href={repoLink}>
-                    <button type="button" className="card__button border-animation">
-                        <AiFillGithub size="1.5em" />
-                        <span className="label">Source Code</span>
-                    </button>    
-                </a>    
             </footer>
         </article>
     )
